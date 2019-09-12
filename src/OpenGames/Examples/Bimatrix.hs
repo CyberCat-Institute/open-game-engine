6b507a320cc101f12814219c5c5b2d713cafcf1a
Philipp Zahn <philipp.zahn@protonmail.com>
2019-09-12 09:23:06 +0200

    Added Meeting in NY with 2 and 3 players

--- src/OpenGames/Examples/Bimatrix.hs
+++ src/OpenGames/Examples/Bimatrix.hs
@@ -13,3 +13,23 @@ matchingPenniesMatrix2 x y = if x == y then 0 else 1
 matchingPennies = reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\(x, y) -> ())) >>> (reindex (\(a1, a2) -> (a1, a2)) ((reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\() -> ((), ())) (\((x, y), ()) -> (x, y))) >>> (reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\x -> x)) &&& ((reindex const (decision "player1" [Heads, Tails]))))))) >>> (fromFunctions (\((), x) -> x) (\(x, y) -> ((x, y), matchingPenniesMatrix1 x y))))) >>> (reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\x -> (x, ())) (\((x, y), ()) -> (x, y))) >>> (reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\x -> x)) &&& ((reindex const (decision "player2" [Heads, Tails]))))))) >>> (fromFunctions (\(x, y) -> (x, y)) (\(x, y) -> ((x, y), matchingPenniesMatrix2 x y))))))))) >>> (fromLens (\(x, y) -> ()) (curry (\((x, y), ()) -> (x, y)))))
 
 matchingPenniesEquilibrium = equilibrium matchingPennies trivialContext
+
+
+-- Meeting in New York
+
+data Coordination = GCT | ES deriving (Eq, Ord, Show)
+meetingInNYMatrix :: Coordination -> Coordination -> Rational
+meetingInNYMatrix x y = if x == y then 1 else 0
+
+
+meetingInNY = reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\(x, y) -> ())) >>> (reindex (\(a1, a2) -> (a1, a2)) ((reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\() -> ((), ())) (\((x, y), ()) -> (x, y))) >>> (reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\x -> x)) &&& ((reindex const (decision "player1" [GCT, ES]))))))) >>> (fromFunctions (\((), x) -> x) (\(x, y) -> ((x, y), meetingInNYMatrix x y))))) >>> (reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\x -> (x, ())) (\((x, y), ()) -> (x, y))) >>> (reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\x -> x)) &&& ((reindex const (decision "player2" [GCT, ES]))))))) >>> (fromFunctions (\(x, y) -> (x, y)) (\(x, y) -> ((x, y), meetingInNYMatrix x y))))))))) >>> (fromLens (\(x, y) -> ()) (curry (\((x, y), ()) -> (x, y)))))
+
+meetingInNYEquilibrium = equilibrium meetingInNY trivialContext
+
+-- Adding a third player
+
+meetingInNYMatrix3 :: Coordination -> Coordination -> Coordination -> Rational
+meetingInNYMatrix3 x y z = if x == y && x == z then 1 else 0
+
+meetingInNY3 = reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\(x, y, z) -> ())) >>> (reindex (\(a1, a2, a3) -> ((a1, a2), a3)) (((reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\() -> ((), ())) (\((x, y, z), ()) -> (x, y, z))) >>> (reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\x -> x)) &&& ((reindex const (decision "player1" [GCT, ES]))))))) >>> (fromFunctions (\((), x) -> x) (\(x, y, z) -> ((x, y, z), meetingInNYMatrix3 x y z))))) >>> (reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\x -> (x, ())) (\((x, y, z), ()) -> (x, y, z))) >>> (reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\x -> x)) &&& ((reindex const (decision "player2" [GCT, ES]))))))) >>> (fromFunctions (\(x, y) -> (x, y)) (\(x, y, z) -> ((x, y, z), meetingInNYMatrix3 x y z)))))) >>> (reindex (\x -> (x, ())) ((reindex (\x -> ((), x)) ((fromFunctions (\(x, y) -> ((x, y), ())) (\((x, y, z), ()) -> (x, y, z))) >>> (reindex (\x -> ((), x)) ((fromFunctions (\x -> x) (\x -> x)) &&& ((reindex const (decision "player3" [GCT, ES]))))))) >>> (fromFunctions (\((x, y), z) -> (x, y, z)) (\(x, y, z) -> ((x, y, z), meetingInNYMatrix3 x y z))))))))) >>> (fromLens (\(x, y, z) -> ()) (curry (\((x, y, z), ()) -> (x, y, z)))))
+meetingInNYEquilibrium3 = equilibrium meetingInNY3 trivialContext
--- src/OpenGames/Examples/Source/Bimatrix.hs
+++ src/OpenGames/Examples/Source/Bimatrix.hs
@@ -10,3 +10,15 @@ matchingPennies = Block [] []
                          Line [] [] "reindex const (decision \"player2\" [Heads, Tails])" ["y"] ["matchingPenniesMatrix2 x y"]]
                         [] []
 
+meetingInNY = Block [] []
+                        [Line [] [] "reindex const (decision \"player1\" [GCT, ES])" ["x"] ["meetingInNYMatrix x y"],
+                         Line [] [] "reindex const (decision \"player2\" [GCT, ES])" ["y"] ["meetingInNYMatrix x y"]]
+                        [] []
+
+
+meetingInNY3 = Block [] []
+                        [Line [] [] "reindex const (decision \"player1\" [GCT, ES])" ["x"] ["meetingInNYMatrix3 x y z"],
+                         Line [] [] "reindex const (decision \"player2\" [GCT, ES])" ["y"] ["meetingInNYMatrix3 x y z"],
+                         Line [] [] "reindex const (decision \"player3\" [GCT, ES])" ["z"] ["meetingInNYMatrix3 x y z"]]
+                        [] []
+
