caec8490abeb87bcd78a1bd80a3413a9a9caa20f
Jules Hedges <j.hedges@qmul.ac.uk>
2019-12-05 20:25:01 +0100

    Committing some half finished stuff so I can fork a public version

---
+++ src/OpenGames/Engine/PolyNumerical.hs
@@ -0,0 +1,10 @@
+module OpenGames.Engine.PolyNumerical where
+
+import Data.Poly
+
+-- Replacing functions with polynomials
+
+-- List of coefficients of a 1-variable polynomial
+newtype Poly = Poly [Double]
+
+newtype PolyLens = PolyLens [(Double, [Double])]
