57e7171bd8fefc4577939c8dd792361260781737
Jules Hedges <j.hedges@qmul.ac.uk>
2020-04-24 17:25:48 +0100

    New backend with state monad for agent identity

---
+++ src/OpenGames/Engine/StatefulPayoffs.hs
@@ -0,0 +1,84 @@
+module OpenGames.Engine.StatefulPayoffs where
+
+-- Puting coplay into a state monad with payoff vectors as a state variable
+-- This augments a game with an imperative program running backwards in time
+-- An experimental pragmatic solution to a practical difficulty with agent identity
+
+import OpenGames.Engine.OpenGamesClass
+import OpenGames.Engine.Diagnostics
+
+import Control.Monad.State -- .Strict would probably be better
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
+-- This could trivially be generalised to any monad
+data StatefulPayoffGame m v a x s y r = StatefulPayoffGame
+  {play        :: a -> x -> y,
+   coplay      :: a -> x -> r -> State v s,
+   equilibrium :: x -> (y -> State v r) -> a -> m}
+
+instance (Monoid m) => OG (StatefulPayoffGame m v) where
+  fromLens v u = StatefulPayoffGame {
+    play        = \() x -> v x,
+    coplay      = \() x r -> return (u x r),
+    equilibrium = \_ _ () -> mempty}
+  reindex f g = StatefulPayoffGame {
+    play        = \a -> play g (f a),
+    coplay      = \a -> coplay g (f a),
+    equilibrium = \h k a -> equilibrium g h k (f a)}
+  (>>>) g1 g2 = StatefulPayoffGame {
+    play        = \(a, b) -> play g2 b . play g1 a,
+    coplay      = \(a, b) x q -> do {r <- coplay g2 b (play g1 a x) q; coplay g1 a x r},
+    equilibrium = \h k (a, b) -> equilibrium g1 h undefined a `mappend` equilibrium g2 (play g1 a h) k b}
+  (&&&) g1 g2 = StatefulPayoffGame {
+    play        = \(a, b) (x1, x2) -> (play g1 a x1, play g2 b x2),
+    coplay      = \(a, b) (x1, x2) (r1, r2) -> do {s1 <- coplay g1 a x1 r1; s2 <- coplay g2 b x2 r2; return (s1, s2)},
+    equilibrium = \(h1, h2) k (a, b) -> equilibrium g1 h1 (\y1 -> do {(r1, _) <- k (y1, play g2 b h2); return r1}) a
+                              `mappend` equilibrium g2 h2 (\y2 -> do {(_, r2) <- k (play g1 a h1, y2); return r2}) b}
+
+-- Operate nonlocally on the payoff of a fixed agent
+agentPayoff :: (Eq agent, Monoid m) => agent -> (p -> r -> p) -> StatefulPayoffGame m (agent -> p) () () () () r
+agentPayoff agent f = StatefulPayoffGame {
+  play        = \() () -> (),
+  coplay      = \() () r -> do {v <- get; put (\agent' -> if agent == agent' then f (v agent) r else v agent')},
+  equilibrium = \() _ () -> mempty}
+
+-- Operate nonlocally on the payoff of an endogenously determined agent
+rolePayoff :: (Eq agent, Monoid m) => (p -> r -> p) -> StatefulPayoffGame m (agent -> p) () agent () () r
+rolePayoff f = StatefulPayoffGame {
+  play        = \() _ -> (),
+  coplay      = \() agent r -> do {v <- get; put (\agent' -> if agent == agent' then f (v agent) r else v agent')},
+  equilibrium = \_ _ () -> mempty}
+
+-- Decision by a fixed agent, adding payoff numerically to state
+agentDecision :: (Eq agent, Show agent, Show x, Show y, Num r, Ord r, Show r)
+              => agent -> [y] -> StatefulPayoffGame [DiagnosticInfo] (agent -> r) (x -> y) x () y r
+agentDecision agent ys = StatefulPayoffGame {
+  play = id,
+  coplay = \_ _ r -> do {v <- get; put (\agent' -> if agent == agent' then v agent + r else v agent')},
+  equilibrium = \h k a -> let u y = let (r, v) = runState (k y) (const 0) in v agent + r
+                              (optimalPlay, optimalPayoff) = maximumBy (comparing snd) [(y, u y) | y <- ys]
+                           in if u (a h) >= optimalPayoff then []
+                                                          else [DiagnosticInfo {player = show agent,
+                                                                                OpenGames.Engine.Diagnostics.state = show h,
+                                                                                strategy = show (a h),
+                                                                                OpenGames.Engine.Diagnostics.payoff = show (u (a h)),
+                                                                                optimalMove = show (optimalPlay),
+                                                                                optimalPayoff = show (optimalPayoff)}]}
+
+-- Decision by an endogenously determined agent, adding payoff numerically to state
+roleDecision :: (Eq agent, Show agent, Show x, Show y, Num r, Ord r, Show r)
+             => [y] -> StatefulPayoffGame [DiagnosticInfo] (agent -> r) (agent -> x -> y) (agent, x) () y r
+roleDecision ys = StatefulPayoffGame {
+  play        = uncurry,
+  coplay      = \_ (agent, _) r -> do {v <- get; put (\agent' -> if agent == agent' then v agent + r else v agent')},
+  equilibrium = \(agent, h) k a -> let u y = let (r, v) = runState (k y) (const 0) in v agent + r
+                                       (optimalPlay, optimalPayoff) = maximumBy (comparing snd) [(y, u y) | y <- ys]
+                                    in if u (a agent h) >= optimalPayoff then []
+                                                                         else [DiagnosticInfo {player = show agent,
+                                                                                               OpenGames.Engine.Diagnostics.state = show h,
+                                                                                               strategy = show (a agent h),
+                                                                                               OpenGames.Engine.Diagnostics.payoff = show (u (a agent h)),
+                                                                                               optimalMove = show (optimalPlay),
+                                                                                               optimalPayoff = show (optimalPayoff)}]}
+
