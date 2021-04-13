7ffcc7c7a87cd53a3478c99a8d62558798cdf5f6
Philipp Zahn <philipp.zahn@protonmail.com>
2021-04-13 20:15:09 +0200

    Reorganization of exports

--- src/Engine/Engine.hs
+++ src/Engine/Engine.hs
@@ -1,8 +1,51 @@
-module Engine.Engine where
+{-# LANGUAGE ExplicitNamespaces #-}
+
+module Engine.Engine
+  ( decision
+  , decisionNoObs
+  , forwardFunction
+  , backwardFunction
+  , natureDraw
+  , liftStochasticForward
+  , StochasticStatefulBayesianOpenGame(..)
+  , Agent(..)
+  , dependentDecision
+  , fromFunctions
+  , fromLens
+  , uniformDist
+  , distFromList
+  , pureAction
+  , DiagnosticInfoBayesian(..)
+  , generateOutput
+  , generateIsEq
+  , OpenGame(..)
+  , lift
+  , reindex
+  , (>>>)
+  , (&&&)
+  , Stochastic(..)
+  , Vector(..)
+  , StochasticStatefulOptic(..)
+  , StochasticStatefulContext(..)
+  , Optic(..)
+  , Precontext(..)
+  , Context(..)
+  , ContextAdd(..)
+  , identity
+  , List(..)
+  , Apply(..)
+  , Unappend(..)
+  , MapL(..)
+  , FoldrL(..)
+  , ConstMap(..)
+  , SequenceList(..)
+  , type (+:+)
+  , (+:+)
+  ) where
 
 -- | File organizes the imports of the engine to streamline the import of relevant functionality
-
-import Engine.BayesianGames hiding (fromFunctions, nature, liftStochastic)
+import Engine.AtomicGames
+import Engine.BayesianGames hiding (nature, liftStochastic)
 import Engine.OpenGames
 import Engine.OpticClass
 import Engine.Diagnostics
