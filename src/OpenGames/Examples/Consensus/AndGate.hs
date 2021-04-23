64e16efd8514db8c5d9a0a01938010cd331902e0
Jules Hedges <jules.hedges@strath.ac.uk>
2021-04-23 16:18:34 +0100

    Fixing some parameters & strategies for and gate game

--- src/OpenGames/Examples/Consensus/AndGate.hs
+++ src/OpenGames/Examples/Consensus/AndGate.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module OpenGames.Examples.Consensus.AndGate where
 
@@ -6,12 +7,14 @@ import Numeric.Probability.Distribution (certainly, uniform, fromFreqs)
 
 import OpenGames.Preprocessor.THSyntax
 import OpenGames.Preprocessor.AbstractSyntax
+import OpenGames.Preprocessor.Compile
 import OpenGames.Engine.OpenGamesClass
 import OpenGames.Engine.OpticClass
 import OpenGames.Engine.StatefulBayesian hiding (decision, roleDecision, dependentDecision)
 import OpenGames.Engine.DependentDecision
 
 import OpenGames.Examples.Consensus.DepositGame (depositStagePlayer, playingStagePlayer, attackerPayoff)
+import Control.Arrow (Kleisli(..))
 
 obfuscateAndGate :: [Bool] -> Bool
 obfuscateAndGate = and
@@ -24,12 +27,65 @@ payoffAndGate penalty reward deposits True
 payoffAndGate penalty reward deposits False
   = [-penalty*deposit | deposit <- deposits]
 
-generateGame "andGateGame" ["numPlayers", "reward", "costOfCapital", "minBribe", "maxBribe", "incrementBribe", "maxSuccessfulAttackPayoff", "attackerProbability", "penalty", "minDeposit", "maxDeposit", "incrementDeposit", "epsilon"] $ Block [] []
-  [Line [ [| replicate numPlayers costOfCapital |] ] ["discard1"] [| population [depositStagePlayer ("Player " ++ show n) minDeposit maxDeposit incrementDeposit epsilon | n <- [1 .. numPlayers]] |] ["deposits"] [ [| replicate numPlayers () |] ],
-   Line [] [] [| nature (fromFreqs [(0, 1 - attackerProbability), (maxSuccessfulAttackPayoff, attackerProbability)]) |] ["successfulAttackPayoff"] [],
-   Line [ [| deposits |], [| successfulAttackPayoff |] ] [] [| dependentDecision "Attacker" (const [minBribe, minBribe+incrementBribe .. maxBribe]) |] ["bribe"] [ [| attackerPayoff bribesAccepted bribe successfulAttackPayoff |] ],
-   Line [ [| replicate numPlayers (deposits, bribe) |] ] ["discard2"] [| population [playingStagePlayer ("Player " ++ show n) [True, False] | n <- [1 .. numPlayers]] |] ["moves"] [ [| zip (payoffAndGate penalty reward deposits (obfuscateAndGate moves)) bribesAccepted |] ],
-   Line [ [| moves |] ] [] [| fromFunctions (map not) id |] ["bribesAccepted"] []]
-  [] []
+unit = ()
 
-andGateGameEq numPlayers reward costOfCapital minBribe maxBribe incrementBribe maxSuccessfulAttackPayoff attackerProbability penalty minDeposit maxDeposit incrementDeposit epsilon = equilibrium (andGateGame numPlayers reward costOfCapital minBribe maxBribe incrementBribe maxSuccessfulAttackPayoff attackerProbability penalty minDeposit maxDeposit incrementDeposit epsilon) void
+successfulAttackPayoffDistribution :: Double -> Double -> Stochastic Double
+successfulAttackPayoffDistribution attackerProbability maxSuccessfulAttackPayoff
+  | (attackerProbability == 0) = certainly 0
+  | (attackerProbability == 1) = certainly maxSuccessfulAttackPayoff
+  | (otherwise) = fromFreqs [(0, 1-attackerProbability), (maxSuccessfulAttackPayoff, attackerProbability)]
+
+andGateGame numPlayers reward costOfCapital minBribe maxBribe incrementBribe maxSuccessfulAttackPayoff attackerProbability penalty minDeposit maxDeposit incrementDeposit epsilon = [opengame|
+  inputs : replicate numPlayers costOfCapital ;
+  feedback : discard1 ;
+  operation : population (map (\n -> depositStagePlayer ("Player " ++ show n) minDeposit maxDeposit incrementDeposit epsilon) [1 .. numPlayers]) ;
+  outputs : deposits ;
+  returns : replicate numPlayers unit ;
+
+  operation : nature (successfulAttackPayoffDistribution attackerProbability maxSuccessfulAttackPayoff) ;
+  outputs : successfulAttackPayoff ;
+
+  inputs : deposits, successfulAttackPayoff ;
+  operation : dependentDecision "Attacker" (const [minBribe, minBribe+incrementBribe .. maxBribe]) ;
+  outputs : bribe ;
+  returns : attackerPayoff bribesAccepted bribe successfulAttackPayoff ;
+
+  inputs : replicate numPlayers (deposits, bribe) ;
+  feedback : discard2 ;
+  operation : population (map (\n -> playingStagePlayer ("Player " ++ show n) [True, False]) [1 .. numPlayers]) ;
+  outputs : moves ;
+  returns : zip (payoffAndGate penalty reward deposits (obfuscateAndGate moves)) bribesAccepted ;
+
+  inputs : moves ;
+  operation : fromFunctions (map not) id ;
+  outputs : bribesAccepted ;
+|]
+
+andGateGameEq =
+  let numPlayers = 2
+      reward = 1.0
+      costOfCapital = 0.05
+      minBribe = 0.0
+      maxBribe = 5.0
+      incrementBribe = 0.05
+      maxSuccessfulAttackPayoff = 1000.0
+      attackerProbability = 1.0
+      penalty = 0.5
+      minDeposit = 0.0
+      maxDeposit = 10.0
+      incrementDeposit = 0.1
+      epsilon = 0.0
+   in equilibrium (andGateGame numPlayers reward costOfCapital minBribe maxBribe incrementBribe maxSuccessfulAttackPayoff attackerProbability penalty minDeposit maxDeposit incrementDeposit epsilon) void
+
+depositStrategy :: [Kleisli Stochastic Double Double]
+depositStrategy = replicate 2 (Kleisli (const (certainly 0.0)))
+
+attackerStrategy :: Kleisli Stochastic ([Double], Double) Double
+attackerStrategy = Kleisli (const (certainly 0.005))
+
+playingStrategy :: [Kleisli Stochastic ([Double], Double) Bool]
+playingStrategy = replicate 2 (Kleisli (\(_, bribe) -> if bribe > 0 then certainly False else certainly True))
+
+-- Next: upgrade playing state to the smart strategy
+-- set attacker probability to something intermediate
+-- then min-max the penalty
