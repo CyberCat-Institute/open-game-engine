78561c41d8b53de4fbce3fbdeaaa112ad17e4b24
Philipp Zahn <philipp.zahn@protonmail.com>
2021-05-03 08:04:12 +0200

    Exporting epsilonDecision

--- src/Engine/BayesianGames.hs
+++ src/Engine/BayesianGames.hs
@@ -11,6 +11,7 @@ module Engine.BayesianGames
   ( StochasticStatefulBayesianOpenGame(..)
   , Agent(..)
   , dependentDecision
+  , dependentEpsilonDecision
   , fromLens
   , fromFunctions
   , nature
