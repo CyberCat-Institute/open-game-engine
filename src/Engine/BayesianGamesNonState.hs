ca69d0d1e00bfa1a47933117f88ade8d4b27d8fb
Philipp Zahn <philipp.zahn@protonmail.com>
2021-08-12 10:26:06 +0200

    Implementation without state payoffs and explicit construction of cont

---
+++ src/Engine/BayesianGamesNonState.hs
@@ -0,0 +1,134 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Engine.BayesianGamesNonState
+  ( StochasticBayesianOpenGame(..)
+  , dependentDecision
+  , dependentEpsilonDecision
+  , fromLens
+  , fromFunctions
+  , nature
+  , liftStochastic
+  , uniformDist
+  , distFromList
+  , pureAction
+  , playDeterministically
+  ) where
+
+
+import           Control.Arrow                      hiding ((+:+))
+import           Control.Monad.State                hiding (state)
+import           Control.Monad.Trans.Class
+import GHC.TypeLits
+
+import Data.Foldable
+import           Data.HashMap                       as HM hiding (null,map,mapMaybe)
+
+
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+import           Data.Utils
+import Numeric.Probability.Distribution hiding (map, lift, filter)
+
+import Engine.OpenGames hiding (lift)
+import Engine.OpticClass
+import Engine.TLL
+import Engine.Diagnostics
+
+---------------------------------------------
+-- Reimplements stateful bayesian from before
+
+type StochasticBayesianOpenGame a b x s y r = OpenGame StochasticOptic StochasticContext a b x s y r
+
+type Agent = String
+
+support :: Stochastic x -> [x]
+support = map fst . decons
+
+bayes :: (Eq y) => Stochastic (x, y) -> y -> Stochastic x
+bayes a y = mapMaybe (\(x, y') -> if y' == y then Just x else Nothing) a
+
+
+deviationsInContext :: (Show x, Show y, Ord y, Show theta)
+                    => Double -> Agent -> x -> theta -> Stochastic y -> (y -> Double) -> [y] -> [DiagnosticInfoBayesian x y]
+deviationsInContext epsilon name x theta strategy u ys
+  = [DiagnosticInfoBayesian { equilibrium = strategicPayoff >= optimalPayoff - epsilon,
+                      player = name,
+                      payoff = strategicPayoff,
+                      optimalMove = optimalPlay,
+                      optimalPayoff = optimalPayoff,
+                      context = u ,
+                      state = x,
+                      unobservedState = show theta,
+                      strategy = strategy}]
+  where strategicPayoff = expected (fmap u strategy)
+        (optimalPlay, optimalPayoff) = maximumBy (comparing snd) [(y, u y) | y <- ys]
+
+
+dependentDecision :: (Eq x, Show x, Ord y, Show y) => String -> (x -> [y]) -> StochasticBayesianOpenGame '[Kleisli Stochastic x y] '[[DiagnosticInfoBayesian x y]] x () y Double
+dependentDecision name ys = OpenGame {
+  play = \(a ::- Nil) -> let v x = do {y <- runKleisli a x; return ((), y)}
+                             u () _ = return ()
+                            in StochasticOptic v u,
+  evaluate = \(a ::- Nil) (StochasticContext h k) ->
+     (concat [ let u y = expected (do {t <- (bayes h x);
+                                       k t y})
+                   strategy = runKleisli a x
+                  in deviationsInContext 0 name x theta strategy u (ys x)
+              | (theta, x) <- support h]) ::- Nil }
+
+dependentEpsilonDecision :: (Eq x, Show x, Ord y, Show y) => Double -> String -> (x -> [y])  -> StochasticBayesianOpenGame '[Kleisli Stochastic x y] '[[DiagnosticInfoBayesian x y]] x () y Double
+dependentEpsilonDecision epsilon name ys = OpenGame {
+  play = \(a ::- Nil) -> let v x = do {y <- runKleisli a x; return ((), y)}
+                             u () _ = return ()
+                            in StochasticOptic v u,
+  evaluate = \(a ::- Nil) (StochasticContext h k) ->
+     (concat [ let u y = expected (do {t <- (bayes h x);
+                                       k t y})
+                   strategy = runKleisli a x
+                  in deviationsInContext epsilon name x theta strategy u (ys x)
+              | (theta, x) <- support h]) ::- Nil }
+
+
+
+-- Support functionality for constructing open games
+fromLens :: (x -> y) -> (x -> r -> s) -> StochasticBayesianOpenGame '[] '[] x s y r
+fromLens v u = OpenGame {
+  play = \Nil -> StochasticOptic (\x -> return (x, v x)) (\x r -> return (u x r)),
+  evaluate = \Nil _ -> Nil}
+
+
+fromFunctions :: (x -> y) -> (r -> s) -> StochasticBayesianOpenGame '[] '[] x s y r
+fromFunctions f g = fromLens f (const g)
+
+nature :: Stochastic x -> StochasticBayesianOpenGame '[] '[] () () x ()
+nature a = OpenGame {
+  play = \Nil -> StochasticOptic (\() -> do {x <- a; return ((), x)}) (\() () -> return ()),
+  evaluate = \Nil _ -> Nil}
+
+liftStochastic :: (x -> Stochastic y) -> StochasticBayesianOpenGame '[] '[] x () y ()
+liftStochastic f = OpenGame {
+  play = \Nil -> StochasticOptic (\x -> do {y <- f x; return ((), y)}) (\() () -> return ()),
+  evaluate = \_ _ -> Nil}
+
+-- Support functionality for stochastic processes (also interface to the probability module in use)
+
+-- uniform distribution
+uniformDist = uniform
+
+-- tailored distribution from a list
+distFromList = fromFreqs
+
+-- pure action (no randomization)
+pureAction x = Kleisli $ const $ certainly x
+
+playDeterministically :: a -> Stochastic a
+playDeterministically = certainly
+
+
---
+++ src/Examples/RepeatedNonState.hs
@@ -0,0 +1,227 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Examples.RepeatedPDNonState where 
+
+
+import           Engine.Engine hiding (StochasticStatefulOptic
+                                      , StochasticStatefulBayesianOpenGame(..)
+                                      , Agent(..)
+                                      , dependentDecision
+                                      , dependentEpsilonDecision
+                                      , fromFunctions
+                                      , fromLens
+                                      , uniformDist
+                                      , distFromList
+                                      , pureAction
+                                      , playDeterministically
+                                      , discount
+                                      )
+import           Engine.BayesianGamesNonState
+import           Preprocessor.Preprocessor
+
+
+import Numeric.Probability.Distribution hiding (map, lift, filter)
+
+-- 1.0. Prisoner's dilemma
+data ActionPD = Cooperate | Defect
+  deriving (Eq, Ord, Show)
+
+-- | Payoff matrix for player i given i's action and j's action
+prisonersDilemmaMatrix :: ActionPD -> ActionPD -> Double
+prisonersDilemmaMatrix Cooperate Cooperate   = 3
+prisonersDilemmaMatrix Cooperate Defect  = 0
+prisonersDilemmaMatrix Defect Cooperate  = 5
+prisonersDilemmaMatrix Defect Defect = 1
+
+discountFactor = 0.5
+
+
+prisonersDilemmaCont = [opengame|
+
+   inputs    : (dec1Old,dec2Old) ;
+   feedback  : (prisonersDilemmaMatrix decisionPlayer1 decisionPlayer2 + return1,prisonersDilemmaMatrix decisionPlayer2 decisionPlayer1 + return2)     ;
+
+   :----------------------------:
+   inputs    :  (dec1Old,dec2Old)    ;
+   feedback  :      ;
+   operation : dependentDecision "player1" (const [Cooperate,Defect]);
+   outputs   : decisionPlayer1 ;
+   returns   : prisonersDilemmaMatrix decisionPlayer1 decisionPlayer2 + return1;
+
+   inputs    :   (dec1Old,dec2Old)   ;
+   feedback  :      ;
+   operation : dependentDecision "player2" (const [Cooperate,Defect]);
+   outputs   : decisionPlayer2 ;
+   returns   : prisonersDilemmaMatrix decisionPlayer2 decisionPlayer1 + return2;
+
+   :----------------------------:
+
+   outputs   : (decisionPlayer1,decisionPlayer2)     ;
+   returns   : (return1,return2)     ;
+  |]
+
+
+
+-- Add strategy for stage game 
+stageStrategy1,stageStrategy2 :: Kleisli Stochastic (ActionPD, ActionPD) ActionPD
+stageStrategy1 = Kleisli $
+   (\case
+       (Cooperate,Cooperate) -> playDeterministically Cooperate
+       (_,_)         -> playDeterministically Defect)
+stageStrategy2 = Kleisli $
+   (\case
+       (Cooperate,Cooperate) -> playDeterministically Cooperate
+       (_,_)         -> playDeterministically Defect)
+
+-- Stage strategy tuple
+strategyTuple = stageStrategy1 ::- stageStrategy2 ::- Nil
+
+-- For a an optic (derived from a play for a given strategy), and state (which is also the action), derive a new StateT
+extractContinuation :: StochasticOptic s (Double,Double) a (Double,Double) -> s -> (Double,Double) -> Stochastic (Double,Double)
+extractContinuation (StochasticOptic v u) x r = do
+  (z,_) <-  (v x)
+  u z r
+
+-- For  an optic (derived from a play for a given strategy), and state derive the action which was played, which in turn is then the next state
+extractNextState :: StochasticOptic s t a b -> s -> Stochastic a
+extractNextState (StochasticOptic v _) x = do
+  (z,a) <- v x
+  pure a
+
+testExtractNextState x = extractNextState execute x  -- this illustrates the problem
+  where execute = play prisonersDilemmaCont strategyTuple
+
+testExtractCont x = extractContinuation execute x (0,0)  -- this illustrates the problem
+  where execute = play prisonersDilemmaCont strategyTuple
+
+
+
+-- What is that payoff? Given a strategy and an action evaluate how the rounds will play out.
+continuationPayoffs
+  :: (Eq t, Num t, Enum t) =>
+     t
+     -> List
+          '[Kleisli Stochastic (ActionPD, ActionPD) ActionPD,
+            Kleisli Stochastic (ActionPD, ActionPD) ActionPD]
+     -> (ActionPD, ActionPD)
+     -> (Double,Double)
+     -> Stochastic (Double,Double)
+continuationPayoffs iterator strat action return@(r1,r2)
+  | iterator == 1 = pure return -- extractContinuation (execute strat) action return
+  | otherwise     = do
+      (r1',r2') <- extractContinuation (execute strat) action (r1, r2)
+      actionNew <-  nextState strat action
+      continuationPayoffs (pred iterator) strat actionNew (r1'*discountFactor,r2'*discountFactor)
+  where execute strat'           = play prisonersDilemmaCont strat' -- results in an optic
+        nextState strat' action' = extractNextState (execute strat') action'
+
+
+-- Gives the context with the initial condition, the state, and for an action taken determines the continuation payoff
+contextCont
+  :: (Eq t1, Num t1, Enum t1) =>
+     t1
+     -> List
+          '[Kleisli Stochastic (ActionPD, ActionPD) ActionPD,
+            Kleisli Stochastic (ActionPD, ActionPD) ActionPD]
+     -> (ActionPD, ActionPD)
+     -> StochasticContext
+          (ActionPD, ActionPD) (Double,Double) (ActionPD, ActionPD) (Double,Double)
+contextCont iterator strat initialAction = StochasticContext (pure ((),initialAction)) (\_ action -> continuationPayoffs iterator strat action (0,0))
+  
+
+
+
+-- evaluate the one stage game with a given strategy and a given initial state, as context, use the payoff derived from continuously playing that stage game
+evaluateIteratedPD iterator strat initialAction = generateOutput $ evaluate prisonersDilemmaCont strat context
+  where context = contextCont iterator strat initialAction
+
+checkEq iterator initialAction = generateIsEq $  evaluate prisonersDilemmaCont strategyTuple context
+  where context = contextCont iterator strategyTuple initialAction
+
+
+
+{--strategyTuple2 = stageStrategy1 ::- stageStrategy2 ::- stageStrategy1 ::- stageStrategy2 ::- Nil
+strategyTuple3 = stageStrategy1 ::- stageStrategy2 ::- stageStrategy1 ::- stageStrategy2 ::- stageStrategy1 ::- stageStrategy2 ::- Nil
+
+-- initial context
+initialContext :: StochasticContext (ActionPD, ActionPD) () (ActionPD, ActionPD) ()
+initialContext = StochasticContext (pure ((),(Cooperate,Cooperate))) (\_ _ -> return ())
+
+-- stageGameEquilibrium
+stageGameEq = evaluate prisonersDilemma strategyTuple initialContext
+
+-- initial context
+initialContext2 :: StochasticContext (ActionPD, ActionPD) () (ActionPD, ActionPD) (Double,Double)
+initialContext2 = StochasticContext (pure ((),(Cooperate,Cooperate))) (\_ x -> return (10,10))
+
+-- stageGameEquilibrium
+stageGameEq2 = evaluate prisonersDilemmaStage strategyTuple initialContext2
+
+initialContext3 :: StochasticContext (ActionPD, ActionPD) (Double,Double) (ActionPD, ActionPD) (Double,Double)
+initialContext3 = StochasticContext (pure ((),(Cooperate,Cooperate))) helperFunction2
+
+
+helperFunction _ (Cooperate,Cooperate) = return (10,0)
+helperFunction _ (_,_)                 = return (0,0)
+
+helperFunction2 _ x = extractContinuation execute x (0,0)  -- this illustrates the problem
+
+  where execute = play prisonersDilemmaCont strategyTuple
+
+-- stageGameEquilibrium
+stageGameEq3 = generateOutput $  evaluate prisonersDilemmaCont strategyTuple initialContext3
+
+prisonersDilemmaStage = [opengame|
+
+   inputs    : (dec1Old,dec2Old) ;
+   feedback  :      ;
+
+   :----------------------------:
+   inputs    :  (dec1Old,dec2Old)    ;
+   feedback  :      ;
+   operation : dependentDecision "player1" (const [Cooperate,Defect]);
+   outputs   : decisionPlayer1 ;
+   returns   : prisonersDilemmaMatrix decisionPlayer1 decisionPlayer2 + cont1;
+
+   inputs    :   (dec1Old,dec2Old)   ;
+   feedback  :      ;
+   operation : dependentDecision "player2" (const [Cooperate,Defect]);
+   outputs   : decisionPlayer2 ;
+   returns   : prisonersDilemmaMatrix decisionPlayer2 decisionPlayer1 + cont2;
+
+   :----------------------------:
+
+   outputs   : (decisionPlayer1,decisionPlayer2)     ;
+   returns   : (cont1,cont2)     ;
+  |]
+
+prisonersDilemma = [opengame|
+
+   inputs    : (dec1Old,dec2Old) ;
+   feedback  :      ;
+
+   :----------------------------:
+   inputs    :  (dec1Old,dec2Old)    ;
+   feedback  :      ;
+   operation : dependentDecision "player1" (const [Cooperate,Defect]);
+   outputs   : decisionPlayer1 ;
+   returns   : prisonersDilemmaMatrix decisionPlayer1 decisionPlayer2 ;
+
+   inputs    :   (dec1Old,dec2Old)   ;
+   feedback  :      ;
+   operation : dependentDecision "player2" (const [Cooperate,Defect]);
+   outputs   : decisionPlayer2 ;
+   returns   : prisonersDilemmaMatrix decisionPlayer2 decisionPlayer1 ;
+
+   :----------------------------:
+
+   outputs   : (decisionPlayer1,decisionPlayer2)     ;
+   returns   :      ;
+  |]
+
+
+-}
