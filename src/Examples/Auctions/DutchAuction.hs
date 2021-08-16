59fbda54091587a2747b24adb0abdc5b2e2189b8
Philipp Zahn <philipp.zahn@protonmail.com>
2021-08-16 11:43:57 +0200

    Working version - Testing WIP

--- src/Examples/Auctions/DutchAuction.hs
+++ src/Examples/Auctions/DutchAuction.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
@@ -16,10 +17,12 @@ import Preprocessor.Preprocessor
 import Examples.Auctions.AuctionSupportFunctions
 
 
+import           Control.Monad.State  hiding (state,void)
+import qualified Control.Monad.State  as ST
 
 -- Create super-game including a tick-repeated version
 -- 
-{-
+
 ----------
 -- A Model
 ----------
@@ -29,7 +32,26 @@ import Examples.Auctions.AuctionSupportFunctions
 
 type Values = Double
 
-values = [20,30,60]
+type Started = Bool
+
+-- fix overall clock - length of auction
+clock = 60
+
+hasTypeBeenDrawn :: Integer -> Bool
+hasTypeBeenDrawn ticker =
+  if ticker < clock then True else False
+
+ignoreUpdateTypes' :: (Started, Double, Double,Double,Double) ->  (Double,Double)
+ignoreUpdateTypes' (True, old1, old2, _ , _) = (old1,old2)
+ignoreUpdateTypes' (False, _,_ , new1, new2) = (new1,new2)
+
+ignoreUpdateTypes :: (Started,Double, Double,Double,Double) -> (Double,Double)
+ignoreUpdateTypes _ = (1,1)
+
+
+
+
+values = [10,20,30]
 
 data Bid = Stop | Wait
   deriving (Eq,Show,Ord)
@@ -55,75 +77,67 @@ natureDrawsTypeStage = [opengame|
     returns   :    ;
   |]
 
--- Draws two values
+-- Given ticker, determine if game is already running. If so, feedforward the old values. If no, draw values and forward them.
+nature2 :: OpenGame
+                     StochasticStatefulOptic
+                     StochasticStatefulContext
+                     ('[])
+                     ('[])
+                     (Integer,Double,Double)
+                     ()
+                     (Double,Double)
+                     ()
+
 nature2 = [opengame|
 
-    inputs    :   ;
+    inputs    : ticker, valueOld1, valueOld2 ;
     feedback  :   ;
 
     :-----:
-    inputs    :   ;
+    inputs    :  ticker ;
     feedback  :   ;
-    operation : nature (uniformDist values) ;
-    outputs   : value1 ;
+    operation : forwardFunction hasTypeBeenDrawn ;
+    outputs   : started ;
     returns   :  ;
 
     inputs    :   ;
     feedback  :   ;
     operation : nature (uniformDist values) ;
-    outputs   : value2 ;
+    outputs   : valueNew1 ;
     returns   :  ;
-
-    :-----:
-
-    outputs   :  value1, value2 ;
-    returns   :    ;
-  |]
-
--- Creates an input parameter and puts into an open game
--- Purpose is for the initial context
-startingTimer timer = [opengame|
 
     inputs    :   ;
     feedback  :   ;
-
-    :-----:
-    inputs    : timer  ;
-    feedback  :   ;
-    operation : forwardFunction (\x -> x) ;
-    outputs   : timer' ;
+    operation : nature (uniformDist values) ;
+    outputs   : valueNew2 ;
     returns   :  ;
 
+    inputs    :  started, valueOld1, valueOld2, valueNew1, valueNew2 ;
+    feedback  :   ;
+    operation : forwardFunction ignoreUpdateTypes' ;
+    outputs   : valuesNew ;
+    returns   :  ;
     :-----:
 
-    outputs   :  timer' ;
+    outputs   : valuesNew ;
     returns   :    ;
   |]
 
--- Creates an input parameter and puts into an open game
--- Purpose is for the initial context
-startingBid bid = [opengame|
-
-    inputs    :   ;
-    feedback  :   ;
-
-    :-----:
-    inputs    : bid  ;
-    feedback  :   ;
-    operation : forwardFunction (\x -> x) ;
-    outputs   : bid' ;
-    returns   :  ;
-
-    :-----:
-
-    outputs   : bid' ;
-    returns   :    ;
-  |]
-
+ 
 -- 1.2. actual bidding parts
-  
 -- Have a base game, and iterate that game.
 -- Individual bidding stage
+biddingStage
+  :: String
+     -> OpenGame
+          StochasticStatefulOptic
+          StochasticStatefulContext
+          '[Kleisli Stochastic (Double, Integer, Bid) Bid]
+          '[[DiagnosticInfoBayesian (Double, Integer, Bid) Bid]]
+          (Double, Integer, Bid)
+          ()
+          Bid
+          Double
 biddingStage name = [opengame|
 
     inputs    :  value, ticker, decs  ;
@@ -142,13 +156,21 @@ biddingStage name = [opengame|
   |]
 
 
--- generate game with 2 players
-biddingGame2  = [opengame|
 
-    inputs    :  value1, value2, ticker, decs  ;
+-- generate game with 2 players
+biddingGameComplete  = [opengame|
+
+    inputs    :  value1Old, value2Old, ticker, decs  ;
     feedback  :   ;
 
     :---------------------------:
+    inputs    :  ticker, value1Old, value2Old   ;
+    feedback  :   ;
+    operation :  nature2 ;
+    outputs   :  (value1, value2) ;
+    returns   :  ;
+
+
     inputs    :  value1, ticker, decs  ;
     feedback  :   ;
     operation :  biddingStage "Player1" ;
@@ -181,18 +203,21 @@ biddingGame2  = [opengame|
 
     :---------------------------:
 
-    outputs   :  value1, value2, newTicker, decsNew ;
+    outputs   :  value1, value2, (newTicker - 1), decsNew ;
     returns   :   ;
   |]
 
+
+  
 ------------------------
 -- 2 Auxiliary functions
+determinePayments :: (Integer,Bid,Bid,Bid) -> (Double,Double)
 determinePayments (ticker,decs,dec1,dec2)
   | decs == Stop = (0,0) -- if the auction was stopped before, payoff is zero
   | ticker == 0  = (0,0) -- if the clock is over, payoff is zero for players
-  | dec1 == Stop && dec2 == Stop = (ticker,0) -- TODO: make this stochastic; default now that the first player gets the good
-  | dec1 == Stop = (ticker,0) -- first player stops the auction
-  | dec2 == Stop = (0,ticker) -- second player stops the auction
+  | dec1 == Stop && dec2 == Stop = (fromInteger ticker,0) -- FIXME: make this stochastic; default now that the first player gets the good
+  | dec1 == Stop = (fromInteger ticker,0) -- first player stops the auction
+  | dec2 == Stop = (0,fromInteger ticker) -- second player stops the auction
   | otherwise    = (0,0)      -- none of the players stops the auction
 
 payoff1 value (pay1,pay2)
@@ -210,80 +235,60 @@ transformDecisions (decs,dec1,dec2) =
      then Stop
      else Wait
 
------------------------------
--- 3 Constructing the context
+-----------------------
+-- Continuation payoffs
+
+-- Extract continuation
+extractContinuation :: StochasticStatefulOptic (Double, Double, Integer, Bid) () (Double, Double, Integer, Bid) () -> (Double, Double, Integer, Bid) -> StateT Vector Stochastic ()
+extractContinuation (StochasticStatefulOptic v u) x = do
+  (z,a) <- ST.lift (v x)
+  u z ()
+
+-- Extract next state (action)
+extractNextState :: StochasticStatefulOptic (Double, Double, Integer, Bid) () (Double, Double, Integer, Bid) () -> (Double, Double, Integer, Bid) -> Stochastic (Double, Double, Integer, Bid)
+extractNextState (StochasticStatefulOptic v _) x = do
+  (z,a) <- v x
+  pure a
 
 
--- I need some starting context which determines the value types
--- Maybe I can create a context out of the play function?
-
-testEvaluate context strat = evaluate biddingGame2 strat context
-testPlay strat = play biddingGame2 strat
-
-
--- Given an optic and a state, extract the action.
-extractForwardLensPart :: StochasticStatefulOptic
-                             (Double, Double, Double, Bid)
-                             ()
-                             (Double, Double, Double, Bid)
-                             ()
-                       -> (Double, Double, Double, Bid)
-                       -> Stochastic ((),(Double, Double, Double, Bid))
-extractForwardLensPart (StochasticStatefulOptic
-                          (v :: (Double, Double, Double, Bid)
-                             -> Stochastic (z, (Double, Double, Double, Bid))) _) s = do
-    (_,a) <- v s
-    return ((),a)
-
--- Turn this into a context
--- Given an optic and a state, produce a context
-opticToContext :: Show z => StochasticStatefulOptic
-                             (Double, Double, Double, Bid)
-                             ()
-                             (Double, Double, Double, Bid)
-                             ()
-                       -> (Double, Double, Double, Bid)
-                       -> StochasticStatefulContext
-                             (Double, Double, Double, Bid)
-                             ()
-                             (Double, Double, Double, Bid)
-                             ()
-opticToContext optic s =
-  let a = extractForwardLensPart optic s
-      in StochasticStatefulContext a (\_ _-> pure ())
-
--- With an initial state, with a strategy, we can produce the next context.
-
-testContext strat initialS = opticToContext (testPlay strat) initialS
-
--- Now let us plug this together with the play functionality
-playToAction strat s = extractForwardLensPart (testPlay strat) s
-
--- Fix a context by hand first
-contextFixed :: StochasticStatefulContext (Double, Double, Double, Bid) () (Double, Double, Double, Bid) ()
-contextFixed = StochasticStatefulContext (pure ((),(30,30,60,Wait))) (\_ _-> pure ())
-
--- create context through play
--- But this requires mapping an optic to context
--- and then give manual numbers for the ticker start
--- focus on evaluating this first
+-- Determine continuation payoff with the same repeated strategy
+determineContinuationPayoffs 1        strat action = pure ()
+determineContinuationPayoffs iterator strat action = do
+   extractContinuation executeStrat action
+   nextInput <- ST.lift $ extractNextState executeStrat action
+   determineContinuationPayoffs (pred iterator) strat nextInput
+ where executeStrat =  play biddingGameComplete strat
 
 
 
-playStartingTimer = play  (startingTimer 60) Nil
+----------
+-- Context
 
-playStartingBid   = play (startingBid Wait) Nil
+-- Context used for the evaluation of the pathological end state
+contextCont iterator strat initialAction = StochasticStatefulContext (pure ((),initialAction)) (\_ action -> determineContinuationPayoffs iterator strat action)
+
+-------------
+-- Strategies
+
+-- Add strategy for stage game
+stageStrategy :: Kleisli Stochastic (Double, Integer, Bid) Bid
+stageStrategy = Kleisli $
+   (\case
+       (_,_,Stop)   -> playDeterministically Stop
+       (v,ticker,_) -> if v < fromInteger ticker then playDeterministically Wait else playDeterministically Stop)
+
+-- Aggregate the strategies into a profile
+strategyTuple = stageStrategy ::- stageStrategy ::- Nil
+
+--------------
+-- Equilibrium
+
+-- Pathological end state
+repeatedCompleteGameEq iterator strat initialAction = evaluate biddingGameComplete strat context
+  where context  = contextCont iterator strat initialAction
+
+-- Show output pathological end game
+eqOutput iterator strat initialAction = generateIsEq $ repeatedCompleteGameEq iterator strat initialAction
 
 
---aggregateOptic = testPlay >>>> playStartingTimer
--- StochasticStatefulContext
---          (Double, Double, Double, Bid) () (Double, Double, Double, Bid) ()
---           x () r ()
 
--- the initial optic gives () () r ()
--- StochasticStatefulContext :: (Show z) => Stochastic (z, s) -> (z -> a -> StateT Vector Stochastic b) -> StochasticStatefulContext s t a b
-
-
-
-testLens x = lens (\y -> x) (\_ _ -> ())
---}
