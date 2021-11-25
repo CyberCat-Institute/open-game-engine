39f8c94bafb605d6034dc1ada5b97d4621c5ab70
Philipp Zahn <philipp.zahn@protonmail.com>
2021-11-25 16:57:50 +0100

    Experimental monte carlo

---
+++ src/Engine/IOGames.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Engine.IOGames
+  ( IOOpenGame(..)
+  , Agent(..)
+  , dependentDecisionIO
+  , fromLens
+  , fromFunctions
+  ) where
+
+
+import           Control.Arrow                      hiding ((+:+))
+import           Control.Monad.Bayes.Weighted
+import           Control.Monad.State                hiding (state)
+import           Control.Monad.Trans.Class
+import GHC.TypeLits
+
+import Data.Foldable
+import           Data.HashMap                       as HM hiding (null,map,mapMaybe)
+
+
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+import           Data.Utils
+import Data.Vector.Generic                         as G
+import System.Random.MWC.CondensedTable
+import System.Random
+
+import Engine.OpenGames hiding (lift)
+import Engine.OpticClass
+import Engine.TLL
+import Engine.Diagnostics
+
+---------------------------------------------
+-- Reimplements stateful bayesian from before
+
+type IOOpenGame a b x s y r = OpenGame (MonadOptic IO) (MonadContext IO) a b x s y r
+
+type Agent = String
+
+-- NOTE we keep the _evaluate_ part undefined as we do not need it.
+dependentDecisionIO :: (Eq x, Show x, Ord y, Show y) => String ->  IOOpenGame '[CondensedTableV Double] '[] x () y Double
+dependentDecisionIO name = OpenGame {
+  play = \(a ::- Nil) -> let v x = do pure ((),undefined)
+                             in MonadOptic v (\_ -> (\_ -> pure ())),
+  evaluate = undefined }
+
+
+-- Support functionality for constructing open games
+fromLens :: (x -> y) -> (x -> r -> s) -> IOOpenGame '[] '[] x s y r
+fromLens v u = OpenGame {
+  play = \Nil -> MonadOptic (\x -> return (x, v x)) (\x r -> return (u x r)),
+  evaluate = \Nil _ -> Nil}
+
+
+fromFunctions :: (x -> y) -> (r -> s) -> IOOpenGame '[] '[] x s y r
+fromFunctions f g = fromLens f (const g)
+
+
+transformProb :: Weighted m (a,b) -> a -> Weighted m b
+transformProb tab x= do
+  tab' <- runWeighted tab
+  let reduced = Prelude.filter ((==x) . fst . fst) tab'
+  withWeight reduced
+
+{-
+-----------------------------
+-- Prob support functionality
+
+-- sample from a distribution: genFromTable
+
+-- update a condensed table by setting values to zero.
+
+updateTable :: Eq a => CondensedTableV (a,b) -> a -> CondensedTableV b
+updateTable (CondensedTable na aa nb bb nc cc dd) = let
+  tbl = G.filter ((== x) . fst) dd
+  in undefined -- tbl
+
+normalize :: CondensedTableV b -> CondensedTableV b
+normalize table
+      | G.null table = error "empty table"
+      | otherwise = G.map (second (/ s)) table
+      where
+        s = G.foldl' (flip $ (+) . snd) 0 table
+--}
---
+++ src/Examples/Markov/TestSimpleMonteCarlo.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Examples.Markov.TestSimpleMonteCarlo where
+
+import           Debug.Trace
+import           Engine.Engine
+import           Preprocessor.Preprocessor
+import           Examples.SimultaneousMoves (ActionPD(..),prisonersDilemmaMatrix)
+
+import           Control.Monad.State  hiding (state,void)
+import qualified Control.Monad.State  as ST
+
+import Numeric.Probability.Distribution hiding (map, lift, filter)
+
+prisonersDilemma  :: OpenGame
+                              StochasticStatefulOptic
+                              StochasticStatefulContext
+                              ('[Kleisli Stochastic (ActionPD, ActionPD) ActionPD,
+                                 Kleisli Stochastic (ActionPD, ActionPD) ActionPD])
+                              ('[[DiagnosticInfoBayesian (ActionPD, ActionPD) ActionPD],
+                                 [DiagnosticInfoBayesian (ActionPD, ActionPD) ActionPD]])
+                              (ActionPD, ActionPD)
+                              ()
+                              (ActionPD, ActionPD)
+                              ()
+discountFactor = 0.5
+
+prisonersDilemma = [opengame|
+
+   inputs    : (dec1Old,dec2Old) ;
+   feedback  :      ;
+
+   :----------------------------:
+   inputs    :  (dec1Old,dec2Old)    ;
+   feedback  :      ;
+   operation : dependentDecision "player1" (const [Cooperate,Defect]);
+   outputs   : decisionPlayer1 ;
+   returns   : prisonersDilemmaMatrix decisionPlayer1 decisionPlayer2 ;
+
+   inputs    :   (dec1Old,dec2Old)   ;
+   feedback  :      ;
+   operation : dependentDecision "player2" (const [Cooperate,Defect]);
+   outputs   : decisionPlayer2 ;
+   returns   : prisonersDilemmaMatrix decisionPlayer2 decisionPlayer1 ;
+
+   operation : discount "player1" (\x -> x * discountFactor) ;
+
+   operation : discount "player2" (\x -> x * discountFactor) ;
+
+   :----------------------------:
+
+   outputs   : (decisionPlayer1,decisionPlayer2)     ;
+   returns   :      ;
+  |]
+
+
+
+-- Add strategy for stage game
+stageStrategy :: Kleisli Stochastic (ActionPD, ActionPD) ActionPD
+stageStrategy = Kleisli $
+   (\case
+       (Cooperate,Cooperate) -> playDeterministically Cooperate
+       (_,_)         -> playDeterministically Defect)
+-- Stage strategy tuple
+strategyTuple = stageStrategy ::- stageStrategy ::- Nil
+
+
+-- extract continuation
+extractContinuation :: StochasticStatefulOptic s () a () -> s -> StateT Vector Stochastic ()
+extractContinuation (StochasticStatefulOptic v u) x = do
+  (z,a) <- ST.lift (v x)
+  u z ()
+
+-- extract next state (action)
+extractNextState :: StochasticStatefulOptic s () a () -> s -> Stochastic a
+extractNextState (StochasticStatefulOptic v _) x = do
+  (z,a) <- v x
+  pure a
+
+executeStrat strat =  play prisonersDilemma strat
+
+
+-- determine continuation for iterator, with the same repeated strategy
+determineContinuationPayoffs :: Integer
+                             -> List
+                                      '[Kleisli Stochastic (ActionPD, ActionPD) ActionPD,
+                                        Kleisli Stochastic (ActionPD, ActionPD) ActionPD]
+                             -> (ActionPD,ActionPD)
+                             -> StateT Vector Stochastic ()
+determineContinuationPayoffs 1        strat action = pure ()
+determineContinuationPayoffs iterator strat action = do
+   extractContinuation executeStrat action
+   nextInput <- ST.lift $ extractNextState executeStrat action
+   determineContinuationPayoffs (pred iterator) strat nextInput
+ where executeStrat =  play prisonersDilemma strat
+
+
+-- fix context used for the evaluation
+contextCont iterator strat initialAction = StochasticStatefulContext (pure ((),initialAction)) (\_ action -> trace ",,1" (determineContinuationPayoffs iterator strat action))
+
+
+
+repeatedPDEq iterator strat initialAction = evaluate prisonersDilemma strat context
+  where context  = contextCont iterator strat initialAction
+
+
+
+
+
+eqOutput iterator strat initialAction = generateIsEq $ repeatedPDEq iterator strat initialAction
