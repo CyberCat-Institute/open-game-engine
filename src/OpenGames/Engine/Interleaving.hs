1924d69c023bc4bb8a7e250ccdc9ad05f349b378
Jules Hedges <jules.hedges@strath.ac.uk>
2022-10-05 13:27:41 +0100

    implement interleaving as fold

--- src/OpenGames/Engine/Interleaving.hs
+++ src/OpenGames/Engine/Interleaving.hs
@@ -11,6 +11,27 @@ import OpenGames.Engine.TLL
 import OpenGames.Engine.OpenGames
 import OpenGames.Engine.OpticClass
 
+foldseq :: [StochasticStatefulOptic (s, [a]) t a b] -> StochasticStatefulOptic [s] [t] [a] [b]
+foldseq = foldr foldseqcons foldseqnil
+
+foldseqcons :: StochasticStatefulOptic (s, [a]) t a b
+            -> StochasticStatefulOptic [s] [t] [a] [b]
+            -> StochasticStatefulOptic [s] [t] [a] [b]
+foldseqcons (StochasticStatefulOptic v1 u1) (StochasticStatefulOptic v2 u2)
+  = StochasticStatefulOptic (\(s:ss) -> do { (z2, as) <- v2 ss
+                                           ; (z1, a) <- v1 (s, as)
+                                           ; return ((z1, z2), a : as) })
+                            (\(z1, z2) (b:bs) -> do { t <- u1 z1 b ; ts <- u2 z2 bs ; return (t:ts) })
+
+foldseqnil :: StochasticStatefulOptic [s] [t] [a] [b]
+foldseqnil = lens (\[] -> []) (\[] [] -> [])
+
+test1 :: StochasticStatefulOptic (Int, [String]) () String ()
+test1 = StochasticStatefulOptic (\(n, ss) -> return ((), (if null ss then "" else head ss) ++ show n)) (\() () -> return ())
+test2 :: StochasticStatefulOptic (Int, [String]) () String ()
+test2 = StochasticStatefulOptic (\(n, ss) -> return ((),show (length ss))) (\() () -> return ())
+
+{-}
 interleave2 :: (Optic o, Context c o, Unappend a, Unappend a')
            => OpenGame o c a b (x, Maybe y') s y r
            -> OpenGame o c a' b' (x', Maybe y) s' y' r'
@@ -38,3 +59,4 @@ interleave2 g h = [opengame|
   outputs : y, y' ;
   returns : r, r' ;
 |]
+-}
