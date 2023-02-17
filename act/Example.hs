c99a904d5792a1a3b7678325ecfa187d5127c8de
Andre Videla <andre.videla@gmail.com>
2023-02-17 13:09:00 +0000

    print content of contract

--- act/Example.hs
+++ act/Example.hs
@@ -6,6 +6,7 @@ import Data.FileEmbed
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 (unpack)
 import Data.Validation
+import Error
 
 import Parse
 import Lex
@@ -15,4 +16,26 @@ import CLI
 actSource :: Data.ByteString.ByteString
 actSource = $(embedFile "amm.act")
 
+compiled :: Error String [Claim]
 compiled =  compile (unpack actSource)
+
+display ::  String
+display = case compiled of
+  Success s -> unlines (fmap printClaim s)
+
+printClaim :: Claim -> String
+printClaim (C constructor) = "constructor"
+printClaim (B b) = "behaviour: " ++ unlines (fmap show (_preconditions b))
+printClaim (I i) = "invariant: " ++ show i
+printClaim (S s) = "Storage: " ++ show s
+
+printConstructor :: Constructor -> String
+printConstructor (Constructor
+  name
+  mode
+  interface
+  preconditions
+  postconditions
+  initialStorage
+  stateupdates) =
+  "constructor: " ++ name
