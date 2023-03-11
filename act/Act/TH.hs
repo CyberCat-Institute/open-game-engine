9552a0cc3de2da12bd4b0d97afd1f0192fb8dbc4
Andre Videla <andre.videla@gmail.com>
2023-03-11 00:36:41 +0000

    Compile updates into TH
    state updates are compiled into record updates
    ACT expressions are converted to TH expressions
    Argument extractors have been moved to their own module

--- act/Act/TH.hs
+++ act/Act/TH.hs
@@ -19,19 +19,12 @@ import Syntax.Annotated
 import CLI
 
 import Act.TH.Dispatch
+import Act.TH.Extractor
 import Act.TH.State
 import Act.Utils
 import Act.Prelude
 
 import Language.Haskell.TH.Syntax as TH
-
-getDeclId :: Decl -> String
-getDeclId (Decl _ name) = name
-
-getDeclType :: Decl -> AbiType
-getDeclType (Decl ty _) = ty
-
-deriving instance Data AbiType
 
 -- Convert from a an act filepath to a list of top-level declaration
 -- state is converted into a record
@@ -41,7 +34,7 @@ deriving instance Data AbiType
 -- contract.
 -- a single top-level function is created taking in argument
 -- the method dispatcher
-act2OG :: String ->  Q [Dec]
+act2OG :: String -> Q [Dec]
 act2OG filename = do
   file <- embedFile filename
   let compiled :: Error String [Claim]
@@ -144,95 +137,12 @@ getUpdates4Method :: [Behaviour] -> [(String, [Rewrite], Interface)]
 getUpdates4Method behaviors =
     fmap (\x -> (_name x, _stateUpdates x, _interface x)) $ filter (\b -> _mode b == Pass) behaviors
 
--- Given each method in the contract we need to know how to extract the arguments from the
--- arguments' array. for this we create a partial top-level function which matches
--- on the argument array and return the correct number of argument in its expected type
--- as a tuple. The function is then called with the expected tuple match in order to
--- retrieve all the argument in the order expected by the body of the function
--- if a method has interface m1(a int, b uint)
--- then the extract function will look like so:
--- ```
--- extractM1 :: [AbiType] -> (Int, UInt)
--- extractM1 [AbiInt i, AbiUInt j] = (i, j)
--- extractM1 x = error ("expected arguments of type (Int, UInt) but got " ++ show x)
-generateExtractMethods :: [(String, Interface)] -> Q [TH.Dec]
-generateExtractMethods = fmap concat . traverse generateExtract
-  where
-    templateCons :: Q TH.Pat -> Q TH.Pat -> Q TH.Pat
-    templateCons a b = [p| $a : $b |]
 
-    patterns4Interface :: Interface -> Q TH.Pat
-    patterns4Interface (Interface _ types) = foldr templateCons [p| [] |] $ fmap patternForDecl types
-
-    generateExtract :: (String, Interface) -> Q [TH.Dec]
-    generateExtract (name, signature) = do
-      let fnName = "extract" ++ capitalise name
-      sig <- extractorTypeForSignature signature
-      incorrectPatternError <- [|error "unexpected arguments"|]
-      patterns <- patterns4Interface signature
-      pure
-        [ SigD
-            (mkName fnName)
-            sig
-        , FunD
-            (mkName fnName)
-            [Clause
-                [patterns]
-                (NormalB (expression4Interface signature))
-                []
-            , Clause [WildP] (NormalB (incorrectPatternError)) []]
-        ]
-
-    -- Generate a pattern for a given declaration, the declaration tells us the type of the ACT
-    -- variable and therefore the constructor to use for out `AbiType` the name will be used as
-    -- binding variable and used in the body to return the value of that type
-    patternForDecl :: Decl -> Q TH.Pat
-    patternForDecl (Decl ty name) = pure (ConP (constructorNameForType ty) [VarP (mkName name)])
-
-
-    constructorNameForType :: AbiType -> Name
-    constructorNameForType = mkName . show . toConstr
-
-
-    -- Convert an ACT Interface into the tuple of values extracted from the list of arguments
-    -- This implement the extractor function which signature is given by `extractorTypeForSignature`
-    -- if the method takes no argument we return a unit value
-    -- if the method takes one argument, we extract that single value from the list
-    -- If the method takes multiple arguments we bundle each
-    expression4Interface :: Interface -> TH.Exp
-    expression4Interface (Interface _ []) = ConE '()
-    expression4Interface (Interface _ [Decl _ name]) = VarE (mkName name)
-    expression4Interface (Interface _ decls) = TupE (fmap (Just . VarE . mkName . getDeclId) decls)
-
-
-    -- Convert a list of declarations name-type into a single tuple. If there
-    -- is only one declaration then the result is the type of that declaration
-    -- If there are no declarations the argument is a unit
-    convertDecls :: [Decl] -> TH.Type
-    convertDecls [] = ConT ''()
-    convertDecls [Decl ty _] = mapAbiTypes ty
-    convertDecls (Decl ty _ : decls) =
-      foldl (\x y -> AppT x y) (TupleT (length decls + 1) `AppT` mapAbiTypes ty) (fmap (mapAbiTypes . getDeclType) decls)
-
-    -- Convert an ACT interface into a haskell type
-    -- This is purely for extractors so the type will always be of the shape
-    --
-    -- ```
-    -- [AbiType] -> *argument tuple*
-    -- ```
-    --
-    -- The argument tuple is defined by `convertDecls` the implementation of the
-    -- function is handled by `expression4Interface`
-    extractorTypeForSignature :: Interface -> Q Type
-    extractorTypeForSignature (Interface _ decls) = [t|[AbiType] -> $(pure (convertDecls decls)) |]
-
-
-extractArgument4Method :: String -> Interface -> [TH.Dec]
-extractArgument4Method methodName interface = []
-
-argumentExtractorName :: String -> TH.Name
-argumentExtractorName methodName = mkName ("extract" ++ capitalise methodName)
-
+-- Return the pattern for bringing the arguments into scope from the argument extractor
+-- functions
+-- If the method has no argument, the pattern is a unit
+-- If the method has one argument the pattern is just a variable name
+-- If the method has multiple arguments, the pattern is a tuple, one per argument
 bindVariables :: [Decl] -> TH.Pat
 bindVariables [] = VarP '()
 bindVariables [Decl _ name] = VarP (mkName name)
@@ -243,13 +153,17 @@ bindVariables xs = TupP (fmap (VarP . mkName . getDeclId) xs)
 -- each constant is also written as a single `State -> State` update function
 mapMethod2TH :: Interface -> [Rewrite] -> Q TH.Exp
 mapMethod2TH (Interface methodName args) actExp = do
-    body <- [| ($(foldl1 (\e1 e2 -> [|$e2 . $e1|]) (fmap rewriteOne actExp))) $(return (VarE contractState)) |]
+
+    -- body <- [| ($(foldl1 (\e1 e2 -> [|$e2 . $e1|]) (fmap rewriteOne actExp))) $(return (VarE contractState)) |]
+    body <- RecUpdE (VarE contractState) <$> traverse rewriteOne actExp
     let extractor = AppE (VarE (argumentExtractorName methodName)) (VarE contractArgs)
     pure $ LetE [ValD (bindVariables args) (NormalB extractor) []] body
+
     where
-        rewriteOne :: Rewrite -> Q TH.Exp
-        rewriteOne (Constant loc) = pure $ VarE (mkName "undefined")
-        rewriteOne (Rewrite (Update ty loc newValue)) = pure $ VarE (mkName "undefined")
+        rewriteOne :: Rewrite -> Q (Name, TH.Exp)
+        rewriteOne (Constant loc) = pure (mkName "unimplemented", VarE (mkName "undefined"))
+        rewriteOne (Rewrite (Update ty (Item _ nm varName _) newValue)) =
+          (mkName varName, ) <$> mapExp newValue
 
 actSource :: Data.ByteString.ByteString
 actSource = $(embedFile "amm.act")
