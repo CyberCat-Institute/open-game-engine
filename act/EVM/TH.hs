602849e77ebb37646f61dd8f1704dfb9c89e3333
Andre Videla <andre.videla@gmail.com>
2023-06-14 19:42:12 +0100

    prototype

--- act/EVM/TH.hs
+++ act/EVM/TH.hs
@@ -5,6 +5,19 @@ import Data.Text (Text, pack)
 import Data.Text.IO
 import Language.Haskell.TH.Syntax as TH
 import EVM.Solidity
+import EVM.Exec
+import EVM.Types
+import Act.Prelude
+import Control.Monad.Trans.State.Strict (get, State)
+
+injectTransaction :: Transaction -> State VM ()
+injectTransaction = undefined
+
+runState :: [Transaction] -> State VM VM
+runState tr = traverse injectTransaction tr >> run
+
+loadContract :: ByteString -> State VM ()
+loadContract = undefined
 
 -- import a list of contracts as an open game
 evm2OG :: [String] -> Q [Dec]
