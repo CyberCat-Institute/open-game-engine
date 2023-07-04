421ee91cd527c75689718c19ad69963541a46e71
Andre Videla <andre.videla@gmail.com>
2023-07-04 15:46:40 +0100

    finish setting up open game from hevm

--- act/EVM/TH.hs
+++ act/EVM/TH.hs
@@ -1,54 +1,107 @@
-{-# LANGUAGE OverloadedLabels, OverloadedRecordDot #-}
+{-# LANGUAGE DuplicateRecordFields, OverloadedLabels, OverloadedRecordDot #-}
 
 module EVM.TH where
 
-import Prelude hiding (readFile)
+import Prelude hiding (FilePath, readFile)
 import Data.Text (Text, pack)
 import Data.Text.IO
+import Data.Map as Map
 import Data.ByteString (ByteString)
-import Data.Vector  as Vector (Vector, fromList)
+import Data.Vector  as Vector (fromList)
+import Data.Tree.Zipper qualified as Zipper
+
 import Language.Haskell.TH.Syntax as TH
 import EVM.ABI
-import EVM.Solidity
-import EVM.Exec
-import EVM.UnitTest
+import EVM.Solidity (solcRuntime)
 import EVM.Types
+import EVM.FeeSchedule
+import EVM (blankState, initialContract)
 import Act.Prelude (EthTransaction(..))
-import Control.Monad.Trans.State.Strict (get, State)
-
-import Optics.Core hiding (elements)
-import Optics.State
-import Optics.State.Operators
-import Optics.Zoom
-
+import Control.Monad.Trans.State.Strict (State, put)
 
 makeCallData :: EthTransaction -> ByteString
 makeCallData (EthTransaction _ method args _ _) =
   abiMethod method (AbiTuple (Vector.fromList args))
 
-paramsFromTx :: EthTransaction -> TestVMParams
-paramsFromTx = undefined
+emptyVM :: [(Addr, ByteString)] -> VM
+emptyVM contracts = VM
+  { result         = Nothing
+  , state          = blankState
+  , frames         = []
+  , env            = envForContracts contracts
+  , block          = emptyBlock
+  , tx             = emptyTransaction
+  , logs           = []
+  , traces         = Zipper.fromForest mempty
+  , cache          = Cache mempty mempty mempty
+  , burned         = 0
+  , iterations     = mempty
+  , constraints    = []
+  , keccakEqs      = []
+  , allowFFI       = True
+  , overrideCaller = Nothing
+  }
+  where
+  -- question: Is that a reasonable empty first block?
+  emptyBlock :: Block
+  emptyBlock = Block
+    { coinbase    = 0
+    , timestamp   = Lit 0
+    , number      = 0
+    , prevRandao  = 0
+    , maxCodeSize = 0
+    , gaslimit    = 0
+    , baseFee     = 0
+    , schedule    = berlin -- specifically this, what is it suppsoed to be?
+    }
+  emptyTransaction :: TxState
+  emptyTransaction = TxState
+    { gasprice = 0
+    , gaslimit = 0
+    , priorityFee = 0
+    , origin = 0
+    , toAddr = 0
+    , value = Lit 0
+    , substate = emptySubState
+    , isCreate = True
+    , txReversion = mempty
+    }
+  emptySubState :: SubState
+  emptySubState = SubState
+    { selfdestructs = []
+    , touchedAccounts = []
+    , accessedAddresses = mempty
+    , accessedStorageKeys = mempty
+    , refunds = []
+    }
 
-injectTransaction :: EthTransaction -> State VM ()
-injectTransaction trans = do
-  makeTxCall (paramsFromTx trans) (ConcreteBuf (makeCallData trans), [])
+  envForContracts :: [(Addr, ByteString)] -> Env
+  envForContracts contracts = Env
+    { contracts = Map.fromList (fmap (fmap bytecodeToContract) contracts)
+    , chainId = 0
+    , storage = EmptyStore
+    , origStorage = mempty
+    , sha3Crack = mempty
+    }
 
-runState :: [EthTransaction] -> State VM VM
-runState tr = traverse injectTransaction tr >> run
+  bytecodeToContract :: ByteString -> Contract
+  bytecodeToContract = initialContract . RuntimeCode . ConcreteRuntimeCode
 
+-- setup a new VM state from the list of contracts we are using
 loadContracts :: [(Addr, ByteString)] -> State VM ()
-loadContracts = undefined
-
--- - we get an initial vm by calling it's constructor and setting most its
---   fields as empty
--- - env is from VM
--- - change the env to contain all contracts
---
-
+loadContracts contracts =
+  put (emptyVM contracts)
 
 -- import a list of contracts as an open game
-evm2OG :: [String] -> Q [Dec]
-evm2OG files = do
-  files :: [(Text, Text)] <- runIO $ traverse (\filename -> (pack filename,) <$> readFile filename) files
-  -- bytecode <- runIO $ loadContracts (??/a(uncurry solcRuntime) files
-  pure []
+-- - first we read off all the files and translate them into solidity bytecode
+-- - Then we associate each contract to a contract name which
+evm2OG :: Map String [Text] -> IO (State VM ())
+evm2OG contractMap = do
+  files :: [(Text, Text)] <- traverse (\filename -> (pack filename,) <$> readFile filename) (keys contractMap)
+  contracts :: [ByteString] <- traverse (\(nm, body) -> do
+      Just bytecode <- solcRuntime nm body
+      pure bytecode) files
+  let bytecodeMap :: [(Addr, ByteString)] = zip [0 .. ] contracts
+  let newVM = loadContracts bytecodeMap
+  pure newVM
+
