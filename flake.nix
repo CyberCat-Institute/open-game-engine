d5f5394306e825a04ca46ad18462403e91301a1b
dxo <hi@d-xo.org>
2023-09-26 16:37:54 +0200

    flake: act builds now

--- flake.nix
+++ flake.nix
@@ -1,14 +1,15 @@
-# file: flake.nix
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    act.url = "github:ethereum/act/52e99daf3121a4e6a6cb28255e862cf8e83cf4cd";
+    act.flake = false;
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
       imports = [
         inputs.haskell-flake.flakeModule
       ];
@@ -16,22 +17,27 @@
         haskellProjects.default = {
           basePackages = pkgs.haskell.packages.ghc927;
 
-          settings = {
-            # TODO: whats going on here?
-            poly.check = false;
+          packages = {
+            act.source = "${inputs.act}/src";
           };
 
-          # What should haskell-flake add to flake outputs?
-          autoWire = [ "packages" "apps" "checks" ]; # Wire all but the devShell
+          settings = {
+            act.extraTestToolDepends = [pkgs.z3];
+            poly = {
+              # TODO: what's going on here? why are these tests failing? where is this dep even coming from?
+              check = false;
+              broken = false;
+            };
+          };
+
+          # send everything but the devShell to the main flake outputs
+          autoWire = [ "packages" "apps" "checks" ];
         };
 
         devShells.default = pkgs.mkShell {
-          name = "open games devshell";
+          name = "og devshell";
           inputsFrom = [
             config.haskellProjects.default.outputs.devShell
-          ];
-          nativeBuildInputs = with pkgs; [
-            # other development tools.
           ];
         };
       };
